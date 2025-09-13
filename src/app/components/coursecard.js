9dcfdf9f26c564b3edcf4919a427ca92d045335b
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2025-09-13 17:14:07 -0500

    first commit

--- src/app/components/coursecard.js
+++ src/app/components/coursecard.js
@@ -9,9 +9,23 @@ const data = [
   }
 ];
 
-const CourseCard = () => {
+const CourseCard = (props) => {
   return (
-    <div>
+    <div className="px-100 py-2">
+      <div className=" bg-white border-gray-900" style={{ borderRadius: '10px', borderColor: 'black', borderWidth: '2px'}}>
+        <h1 className="flex px-5 text-3xl font-bold text-gray-600 py-4">
+          {props.courseCode}
+        </h1>
+        <h2 className="flex px-5 text-2xl font-semibold text-gray-800 py-2">
+          {props.title}
+        </h2>
+        <div className="flex px-5 text-md text-gray-600 py-2">
+          {props.description}
+        </div>
+        <div className="flex px-5 text-md text-gray-600 py-2 pb-4" >
+          <span className="bg-green-300 text-green-700 " style={{ borderRadius: '15px', padding: 5}}> Credit Hours: {props.creditHours} </span>
+        </div>
+      </div>
       
     </div>
   );
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Link from "next/link";
-import { User, Settings } from "lucide-react";
+import { User, Settings, Bed, BedDouble, BedSingle } from "lucide-react";
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b border-gray-300 text-black">
+            <div className="flex gap-4">
+                <Link href="/" className="relative group">
+                    <Bed size={40} className="cursor-pointer" />
+                    <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left rounded-full pointer-events-none" />
+                </Link>
+            </div>
+            <div className="flex gap-4">
+                <Link href="/settings" className="group relative">
+                    <BedDouble size={40} className="cursor-pointer" />
+                    <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left rounded-full pointer-events-none" />
+                </Link>
+            </div>
+            <div className="flex gap-4">
+                <Link href="/profile" className="group relative">
+                    <BedSingle size={40} className="cursor-pointer" />
+                    <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left rounded-full pointer-events-none" />
+                </Link>
+            </div>
+        </nav>
     );
 }
 
